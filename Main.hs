7dfb5f8a9f3893aac4cd13381b9cb239d1545bee
Russel Arbore <russel@russelarbore.com>
2021-11-04 09:43:28 -0500

    Draw window with line in it

--- Main.hs
+++ Main.hs
@@ -10,5 +10,27 @@
     You should have received a copy of the GNU General Public License
     along with rwm. If not, see <https://www.gnu.org/licenses/>.  -}
 
+import Graphics.X11
+
+blockUntilMapNotify :: Display -> IO ()
+blockUntilMapNotify display = do
+  allocaXEvent $ \e -> do
+    nextEvent display e
+    t <- get_EventType e
+    if (t == mapNotify) then return () else blockUntilMapNotify display
+
 main :: IO ()
-main = do print "Hello world"
+main = do
+  display <- openDisplay ""
+  let black = blackPixel display (defaultScreen display)
+  let white = whitePixel display (defaultScreen display)
+  window <- createSimpleWindow display (defaultRootWindow display) 0 0 200 100 0 black black
+  selectInput display window structureNotifyMask
+  mapWindow display window
+  gc <- createGC display window
+  setForeground display gc white
+  blockUntilMapNotify display
+  drawLine display window gc 10 60 180 20
+  flush display
+  blockUntilMapNotify display
+  print "Hello"
